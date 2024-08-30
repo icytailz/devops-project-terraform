e5ae4a2fd4f29e9e3034674bcc4d38dd797a8b08
icytailz <icy.tailz@gmail.com>
2024-08-30 19:47:30 +0700

    update terraform code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -40,5 +40,50 @@ locals {
 resource "aws_subnet" "public" {
   count = local.create_public_subnets && (local.len_public_subnets >= length(var.azs)) ? local.len_public_subnets : 0
   vpc_id = local.vpc_id
+
+  tags = merge(
+    {
+      Name = try(
+        var.public_subnet_names[count.index], format("${var.name}-${var.public_subnet_suffix}-%s", element(var.azs, count.index))
+      )
+    },
+    var.tags,
+    var.public_subnet_tags,
+    lookup(var.public_subnet_tags_per_az, element(var.azs, count.index),{})
+  )
+}
+
+locals {
+  num_public_route_tables = var.create_multiple_public_route_tables ? local.len_public_subnets : 1
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = local.vpc_id
+  count = local.create_public_subnets ? local.num_public_route_tables : 0
+
+  tags = merge(
+    {
+      "Name" = var.create_multiple_public_route_tables ? format("${var.name}-${var.public_subnet_suffix}-%s", element(var.azs, count.index),) : "${var.name}-${var.public_subnet_suffix}"
+    },
+    var.tags,
+    var.public_route_table_tags,
+  )
+}
+
+resource "aws_route_table_association" "public" {
+  count = local.create_public_subnets ? local.len_public_subnets : 0
+
+  subnet_id = element(aws_subnet.public[*].id, count.index)
+  route_table_id = element(aws_route_table.public[*].id, var.create_multiple_public_route_tables ? count.index : 0)
+}
+
+resource "aws_route" "public_internet_gateway" {
+  count = local.create_public_subnets && var.create_igw ? local.num_public_route_tables : 0
+  route_table_id = aws_route_table.public[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.this[0].id
   
+  timeouts {
+    create = "5m"
+  }
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -9,13 +9,49 @@ variable "public_subnets" {
     default = []
     description = "A list of public subnets inside the vpc"
 }
-
+variable "public_subnet_names" {
+    default = []
+    type = list(string)
+    description = "Name tag for the public subnet. If empty, name tags are generated"
+}
+variable "public_subnet_suffix" {
+    type = string
+    default = "public"
+    description = "Suffix to append to public subnets name"
+}
+variable "public_subnet_tags" {
+    type = map(string)
+    default = {}
+    description = "Additional tags for the public subnets"
+}
+variable "public_subnet_tags_per_az" {
+    type = map(map(string))
+    default = {}
+    description = "Additional tags for the public subnets where the primary key is the AZ"
+}
 variable "private_subnets" {
     type = list(string)
     default = []
     description = "A list of private subnets inside the vpc"
 }
 
+variable "create_multiple_public_route_tables" {
+    type = bool
+    default = false
+    description = "Indicates whether to create a separate route table for each subnet"
+}
+
+variable "public_route_table_tags" {
+    type = map(string)
+    default = {}
+    description = "Additional tags for the public route tables"
+}
+
+variable "create_igw" {
+    type = bool
+    default = true
+    description = "Controls if an internet gateway is created for public subnets and the related routes that connect them"
+}
 variable "azs" {
     description = "List of availability zones by name or id in the region"
     type = list(string)
