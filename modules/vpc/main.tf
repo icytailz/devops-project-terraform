5f9f9e3ec42e0e64263562ca843eb0c0503f135b
victorpham <victorpham@lgcns.com>
2024-09-04 09:30:38 +0700

    update tf files

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,6 +3,7 @@ locals {
 
   len_public_subnets = length(var.public_subnets)
   len_private_subnets = length(var.private_subnets)
+  len_database_subnets    = length(var.database_subnets)
 
   max_subnet_length = max (
     local.len_public_subnets,
@@ -216,5 +217,101 @@ resource "aws_route" "private_nat_gateway" {
   timeouts {
     create = "5m"
   }
-  
+}
+################################################################################
+# Database Subnets
+################################################################################
+
+locals {
+  create_database_subnets     = local.create_vpc && local.len_database_subnets > 0
+  create_database_route_table = local.create_database_subnets && var.create_database_subnet_route_table
+}
+
+resource "aws_subnet" "database" {
+  count = local.create_database_subnets ? local.len_database_subnets : 0
+
+  availability_zone                              = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  availability_zone_id                           = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
+  cidr_block                                     = var.database_subnet_ipv6_native ? null : element(concat(var.database_subnets, [""]), count.index)
+  enable_resource_name_dns_a_record_on_launch    = !var.database_subnet_ipv6_native && var.database_subnet_enable_resource_name_dns_a_record_on_launch
+  private_dns_hostname_type_on_launch            = var.database_subnet_private_dns_hostname_type_on_launch
+  vpc_id                                         = local.vpc_id
+
+  tags = merge(
+    {
+      Name = try(
+        var.database_subnet_names[count.index],
+        format("${var.name}-${var.database_subnet_suffix}-%s", element(var.azs, count.index), )
+      )
+    },
+    var.tags,
+    var.database_subnet_tags,
+  )
+}
+
+resource "aws_db_subnet_group" "database" {
+  count = local.create_database_subnets && var.create_database_subnet_group ? 1 : 0
+
+  name        = lower(coalesce(var.database_subnet_group_name, var.name))
+  description = "Database subnet group for ${var.name}"
+  subnet_ids  = aws_subnet.database[*].id
+
+  tags = merge(
+    {
+      "Name" = lower(coalesce(var.database_subnet_group_name, var.name))
+    },
+    var.tags,
+    var.database_subnet_group_tags,
+  )
+}
+
+resource "aws_route_table" "database" {
+  count = local.create_database_route_table ? var.single_nat_gateway || var.create_database_internet_gateway_route ? 1 : local.len_database_subnets : 0
+
+  vpc_id = local.vpc_id
+
+  tags = merge(
+    {
+      "Name" = var.single_nat_gateway || var.create_database_internet_gateway_route ? "${var.name}-${var.database_subnet_suffix}" : format(
+        "${var.name}-${var.database_subnet_suffix}-%s",
+        element(var.azs, count.index),
+      )
+    },
+    var.tags,
+    var.database_route_table_tags,
+  )
+}
+
+resource "aws_route_table_association" "database" {
+  count = local.create_database_subnets ? local.len_database_subnets : 0
+
+  subnet_id = element(aws_subnet.database[*].id, count.index)
+  route_table_id = element(
+    coalescelist(aws_route_table.database[*].id, aws_route_table.private[*].id),
+    var.create_database_subnet_route_table ? var.single_nat_gateway || var.create_database_internet_gateway_route ? 0 : count.index : count.index,
+  )
+}
+
+resource "aws_route" "database_internet_gateway" {
+  count = local.create_database_route_table && var.create_igw && var.create_database_internet_gateway_route && !var.create_database_nat_gateway_route ? 1 : 0
+
+  route_table_id         = aws_route_table.database[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this[0].id
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+resource "aws_route" "database_nat_gateway" {
+  count = local.create_database_route_table && !var.create_database_internet_gateway_route && var.create_database_nat_gateway_route && var.enable_nat_gateway ? var.single_nat_gateway ? 1 : local.len_database_subnets : 0
+
+  route_table_id         = element(aws_route_table.database[*].id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.this[*].id, count.index)
+
+  timeouts {
+    create = "5m"
+  }
 }
